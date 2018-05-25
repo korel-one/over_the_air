b6c7631f5cfb30417a0f7d012b25fa5ac2861774
Pietro Carta <ptrcarta@gmail.com>
2018-05-25 22:07:09 +0200

    barely functioning FFT transmitter decoder

--- ota2.py
+++ ota2.py
@@ -1,7 +1,7 @@
 import sounddevice as sd
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.fftpack import fft, ifft
+from scipy.fftpack import fft, ifft, fftfreq
 
 def str2bits(str):
     res = bin(int.from_bytes(str.encode('ascii'), 'big'))[2:]
@@ -9,20 +9,33 @@ def str2bits(str):
 
 class SoundCommunication2:
 
-    def __init__(self, FS, symRate, msgSymLen, sync = '0000000011111111'):
+    def __init__(self, FS, symRate, msgSymLen, sync):
+        assert len(sync) % 4 == 0
         self.symRate= symRate
         self.FS = FS
+        #samples per second
+        self.symsamp = np.int(FS/symRate) + np.int(FS/symRate)%2
         self.symlen = msgSymLen
         self.synclen = len(sync)//4
-        self.T = self.create_t(1/symRate)
+        self.T = self.create_t(1/symRate, self.symsamp)
+        shaping = np.empty(self.symsamp)
+        shaping[:self.symsamp//2] = np.linspace(0, 1, self.symsamp//2)
+        shaping[self.symsamp//2:] = shaping[:self.symsamp//2][::-1]
+        self.tunenum = 16
+        self.freqbot = 1000
+        self.freqtop = 2000
+        #TODO set frequencies exactly
+        self.pulsefreq = np.linspace(self.freqbot, self.freqtop, self.tunenum + 2)[1:-1] #discard frequencies
+        self.fr_spacing = self.pulsefreq[1] - self.pulsefreq[0]
+        #too close to edges
         self.modpulse = dict()
-        for i in range(16):
-            self.modpulse[i] = np.sin(self.T * 2 * np.pi * (1015 + i*(1000/16)))
+        for i, freq in enumerate(self.pulsefreq):
+            self.modpulse[i] = np.sin(self.T * 2 * np.pi * freq) * shaping
         self.sync = sync
         # normalize both pulses to same power
 
-    def create_t(self, length_seconds):
-        return np.linspace(0, length_seconds, int(self.FS*length_seconds))
+    def create_t(self, length_seconds, samples):
+        return np.linspace(0, length_seconds, samples)
 
     def chunks(self, msg):
         chunk =''
@@ -33,17 +46,17 @@ class SoundCommunication2:
                 chunk = ''
 
     def send(self, binstream):
-        symLen = len(binstream)//4
-        S = np.zeros(np.int((symLen + self.synclen)* self.FS/self.symRate))
+        total_len = self.synclen + self.symlen
+        S = np.zeros(self.symsamp*total_len)
         msg = self.sync + binstream
 
         chunk = ''
         for i, chunk in enumerate(self.chunks(msg)):
-            S[int(i*self.FS/self.symRate):int((i + 1)*self.FS/self.symRate)]\
+            S[i*self.symsamp:(i+1)*self.symsamp]\
                         += self.modpulse[int(chunk, 2)]
-        #S /= np.max(np.abs(S))
 
         return S
+
 
     def _correlate(self, x, y):
         """periodic correlation"""
@@ -55,27 +68,32 @@ class SoundCommunication2:
 
     def decode(self, W):
         correlating_sync = self.send('')
-        corr = self._correlate(W[:np.int(self.symlen*self.FS/self.symRate)], correlating_sync)
+        corr = self._correlate(W[:(self.symlen + self.synclen) * self.symsamp], correlating_sync)
 
         start_sync = np.argmax(np.abs(corr)) # take the absolute value because the microphone
                                             # could inverse + and -
 
-        start_msg = start_sync + np.int(self.synclen*self.FS/self.symRate)
-        end_msg = start_msg + np.int(self.symlen*self.FS/self.symRate)
+        start_msg = start_sync + self.synclen*self.symsamp
+        end_msg = start_msg + self.symlen*self.symsamp
         W = W[start_msg:end_msg]
         if W.size < end_msg - start_msg:
             #padd to avoid slow processing in fft
             W = np.hstack((W, np.zeros(end_msg - start_msg - W.size)))
-        corr = np.zeros((16, W.size))
-        for tune in range(16):
-            corr[tune] = self._correlate(W, self.modpulse[tune])
-        res = ''
+        res = np.zeros(self.symlen, dtype=np.intp)
+        frqcies = fftfreq(self.symsamp, 1/self.FS)
+        for i in range(self.symlen):
+            Fwindow = fft(W[i*self.symsamp:(i+1)*self.symsamp])
+            freq_power = np.empty(self.pulsefreq.shape)
+            for sym_idx, freq in enumerate(self.pulsefreq):
+                freq_idx = np.argmin(np.abs(frqcies - freq)) #closest frequency
+                assert abs(frqcies[freq_idx] - freq) < self.fr_spacing/3, "frequency not close enough to tune"
+                freq_power[sym_idx] = np.sum(np.abs(Fwindow[freq_idx])**2)
+            res[i] = np.argmax(freq_power)
 
         def tobin(n):
             b = bin(n)[2:]
             return '0'*(4 - len(b)) + b
 
-        sel_corr = corr[:,::int(np.floor(self.FS/self.symRate))]
-        res = ''.join(map(tobin, np.argmax(sel_corr, axis=0)))
+        res_str = ''.join(map(tobin, res))
 
-        return res
+        return res_str
--- testing.py
+++ testing.py
@@ -1,5 +1,7 @@
 import numpy as np
+from matplotlib import pyplot as plt
 from scipy.fftpack import rfft, irfft, rfftfreq
+from scipy.fftpack import fftfreq, fft, ifft
 
 class Tester:
     def __init__(self, communicator):
@@ -26,13 +28,33 @@ class Tester:
 
         # If our original signal time was in seconds, this is now in Hz
         in_band_mask = np.logical_or(
-                np.logical_and(freq < lowerFreq, freq > higherFreq),
+                np.logical_and(freq > lowerFreq, freq < higherFreq),
                 np.logical_and(freq > -higherFreq,freq < -lowerFreq)
                 )
         out_of_band_mask = np.logical_not(in_band_mask)
         f_wnoise[in_band_mask] *= sigma_pass
         f_wnoise[out_of_band_mask] *= sigma_out
         return W + irfft(f_wnoise)[:W.size]
+
+    def plot_frequencies(self, R, fromfrq=900, tofrq=2100):
+        #size of R becomes next power of 2
+        Rold = R
+        v = R.size
+        v-=1
+        v |= v >> 1;
+        v |= v >> 2;
+        v |= v >> 4;
+        v |= v >> 8;
+        v |= v >> 16;
+        v+=1
+        R = np.zeros(v)
+        R[:Rold.size] = Rold
+        freq = np.roll(fftfreq(R.size, 1/self._comm.FS), R.size//2)
+        Rtrans = np.roll(fft(R), R.size//2)
+        b = np.argmax(freq > fromfrq)
+        t = np.argmax(freq > tofrq)
+        plt.plot(freq[b:t], np.abs(Rtrans[b:t]))
+        plt.show()
 
     def addRandomShift(self, W, lowerBoundShift = 0., upperBoundShift = 1.):
         assert lowerBoundShift >= 0.
@@ -42,6 +64,18 @@ class Tester:
         leftover = self._comm.FS*shift - intshift
         movW = np.interp(np.arange(W.size) + leftover, np.arange(W.size), W)
         return np.hstack((np.zeros(intshift), movW))
+
+    def diff(self, a, b):
+        assert len(a) == len(b), "Length is not the same"
+        res = ''
+        err = 0
+        for i, (l, r) in enumerate(zip(self._comm.chunks(a), self._comm.chunks(b))):
+            if l == r:
+                res += l
+            else:
+                res += 'X'*len(l)
+                err+=1
+        return res, err/(i+1)
 
     def sendWithNoiseShift(self, r, sigma_pass, sigma_out, lowerBoundShift = 0., upperBoundShift = 1.):
         R = self._comm.send(r)
