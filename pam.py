62d14e7ca13061280d492286dbb35d555b2d6736
Pietro Carta <ptrcarta@gmail.com>
2018-05-30 17:40:14 +0200

    fixed length at 180 chars, 0-padded at the end

--- pam.py
+++ pam.py
@@ -2,14 +2,16 @@ import sounddevice as sd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.fftpack import fft, ifft, fftfreq
+from scipy.fftpack import rfft, irfft, rfftfreq
 
 def str2bits(str):
     res = bin(int.from_bytes(str.encode('ascii'), 'big'))[2:]
     return '0'*(8 - (len(res) % 8)) + res
 
+#TODO add coding
 class SoundCommunication:
 
-    def __init__(self, FS, symRate, msgSymLen, sync = None):
+    def __init__(self, FS, symRate, freqbot, freqtop, msgSymLen = 180*8, sync = None):
         if sync: print("specified sync discarded")
         sync = '1100001111100011011101101101011001111100100100001000010100111000001000110011110011110110101110100110'
         self.symRate= symRate
@@ -20,15 +22,15 @@ class SoundCommunication:
         self.synclen = len(sync)
         self.T = self.create_t(1/symRate, self.symsamp)
         shaping = np.empty(self.symsamp)
-        shaping[:self.symsamp//2] = np.hstack((np.linspace(0, 1, self.symsamp//16), np.ones(self.symsamp//2 - self.symsamp//16)))
+        shaping[:self.symsamp//2] = np.hstack((np.linspace(0, 1, self.symsamp//8), np.ones(self.symsamp//2 - self.symsamp//8)))
         shaping[self.symsamp//2:] = shaping[:self.symsamp//2][::-1]
 
-        self.freqbot = 1000
-        self.freqtop = 2000
+        self.freqbot = freqbot
+        self.freqtop = freqtop
 
         self.pulse = np.zeros((2, self.symsamp))
-        self.pulse[0] = - shaping*np.sin(self.T * 2 * np.pi * 1500)
-        self.pulse[1] =   shaping*np.sin(self.T * 2 * np.pi * 1500)
+        self.pulse[0] = - shaping*np.sin(self.T * 2 * np.pi * (freqtop + freqbot)/2)
+        self.pulse[1] =   shaping*np.sin(self.T * 2 * np.pi * (freqtop + freqbot)/2)
 
         self.sync = sync
         # normalize both pulses to same power
@@ -40,6 +42,8 @@ class SoundCommunication:
         total_len = self.synclen + self.symlen
         S = np.zeros(self.symsamp*total_len)
         msg = self.sync + binstream
+        assert len(binstream) <= self.symlen
+        binstream = binstream + '0'*(self.symlen - len(binstream))
 
         for i, bit in enumerate(msg):
             S[i*self.symsamp:(i+1)*self.symsamp]\
@@ -56,7 +60,15 @@ class SoundCommunication:
             y[:yold.size] = yold
         return ifft(fft(x) * fft(y).conj()).real
 
+    def bandpass_filter(self, W, low, high):
+        fW = rfft(W)
+        freqs = rfftfreq(W.size, 1/self.FS)
+        mask = np.logical_not(np.logical_and(freqs > low, freqs < high))
+        fW[mask] = 0.
+        return irfft(fW)
+
     def decode(self, W):
+        W = self.bandpass_filter(W, self.freqbot, self.freqtop)
         correlating_sync = self.send('')
         corr = self._correlate(W[:(self.symlen + self.synclen) * self.symsamp], correlating_sync)
 
